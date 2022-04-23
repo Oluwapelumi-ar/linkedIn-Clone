2702e823a0207af0bf5fa6eec90e1f3e374b9942
Shukurah Bakare <shukurahbakare@outlook.com>
2022-04-23 23:46:40 +0100

    styled a section of the sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,8 +2,22 @@ import { Avatar } from '@mui/material';
 import React from 'react';
 import './SideBar.css'
 
+
+
 function SideBar() {
+
+  const recentItem = (topic) => {
+    return (
+      <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+    )
+    
+  }
+  
   return (
+    
     <div className='sidebar'>
       <div className="sidebar__top">
         <img src="https://images.unsplash.com/photo-1621091211034-53136cc1eb32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGJhY2tncm91bmQlMjBpbWFnZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
@@ -19,12 +33,18 @@ function SideBar() {
         </div>
         <div className="sidebar__stat">
           <p>Views on Post</p>
-          <p className='sidebar__statNumber2,543'></p>
+          <p className='sidebar__statNumber'>2,543</p>
         </div>
       </div>
 
       <div className="sidebar__bottom">
         <p>Recent</p>
+        
+        {recentItem('React')}
+        {recentItem('React')}
+        {recentItem('React')}
+        {recentItem('React')}
+        
       </div>
     </div>
   )
