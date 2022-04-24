0ab31944f9bfa789aea133c3e9964bcaa64256e8
Shukurah Bakare <shukurahbakare@outlook.com>
2022-04-24 22:36:17 +0100

    Populated the widget components with data

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,15 +1,38 @@
-import { Info } from '@mui/icons-material';
+import { FiberManualRecord, Info } from '@mui/icons-material';
 import React from 'react';
 import './Widget.css'
 
 function Widget() {
+
+  const newsArticle = (heading, subtitle) => {
+    return (
+      <div className="widgets__article">
+      <div className="widgets__articleLeft">
+        <FiberManualRecord />
+      </div>
+      <div className="widgets__articleRight">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+    )
+    
+  }
+
   return (
     <div className='widgets'>
       <div className="widgets__header">
         <h2>LinkedIN News</h2>
         <Info />
       </div>
+      {newsArticle('Shuks is the next big thing','Top news -   9999 Lovers')}
+      {newsArticle('Shuks is the next big thing','Top news -   9999 Lovers')}
+      {newsArticle('Shuks is the next big thing','Top news -   9999 Lovers')}
+      {newsArticle('Shuks is the next big thing','Top news -   9999 Lovers')}
+      {newsArticle('Shuks is the next big thing','Top news -   9999 Lovers')}
+      
     </div>
+
   )
 }
 
