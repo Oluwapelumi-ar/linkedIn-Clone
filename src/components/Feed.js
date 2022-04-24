acf268a013754c2ece72c1c14684e544e742a0db
Shukurah Bakare <shukurahbakare@outlook.com>
2022-04-24 13:32:28 +0100

    created a component for the post section and styled it

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,9 +1,18 @@
 import { CalendarViewDay, Create, EventNote, Image, Subscriptions } from '@mui/icons-material';
-import React from 'react';
+import React, { useState } from 'react';
 import './Feed.css';
 import InputOption from './InputOption';
+import Post from './Post';
 
 function Feed() {
+
+  const [posts,setPosts] = useState([]);
+
+  const sendPost = (event) => {
+    event.preventDefault();
+    console.log(event.target.value)
+  }
+
   return (
     <div className='feed'>
       <div className="feed__inputContainer">
@@ -11,7 +20,7 @@ function Feed() {
           <Create />
           <form >
             <input type='text' />
-            <button type='submit'>Send</button>
+            <button type='submit' onClick={sendPost}>Send</button>
           </form>
         </div>
         <div className="feed__inputOptions">
@@ -21,9 +30,11 @@ function Feed() {
           <InputOption Icon={CalendarViewDay} title='Write Article' color='#7FC15E'/>
         </div>
       </div>
-
+      {posts.map((post) => {
+        <Post />
+      })}
       <div className="post">
-        
+        <Post  name='Shukrah  Bakare' description='This is a text' message='WOW this work' photoUrl=''/>
       </div>
     </div>
   )
